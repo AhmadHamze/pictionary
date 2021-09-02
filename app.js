eb2050f67f8b8092d065f4e43f834bfafde1ee05
Ahmad Hamze <ahmadhamze@yahoo.com>
2021-09-02 14:09:32 +0300

    completion

--- app.js
+++ app.js
@@ -10,4 +10,10 @@ const io = socket(server);
 
 io.sockets.on("connection", (socket) => {
   console.log(`new connection: ${socket.id}`);
+  socket.on("coordinates", (data) => {
+    console.log(data);
+    // Sending back the coordinates to the client, notice this way the
+    // client sending the coordinates will not receive back what it already sent.
+    socket.broadcast.emit("coordinates", data);
+  });
 });
--- public/sketch.js
+++ public/sketch.js
@@ -5,12 +5,31 @@ function setup() {
   canvas.parent("container");
   background(0);
   socket = io.connect("http://localhost:5000");
+  // the socket receiving data from the back-end
+  socket.on("coordinates", newDrawing);
 }
 
-function draw() {
+function newDrawing(data) {
+  noStroke();
+  fill(0, 255, 0);
+  ellipse(data.x, data.y, 20, 20);
+}
+
+function pen() {
   noStroke();
   if (mouseIsPressed) {
     fill(0, 0, 255);
     ellipse(mouseX, mouseY, 20, 20);
+
+    const data = {
+      x: mouseX,
+      y: mouseY,
+    };
+    // Sending the data under the name "coordinates"
+    socket.emit("coordinates", data);
   }
+}
+
+function draw() {
+  pen();
 }
